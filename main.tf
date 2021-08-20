ed746d6c5737dae40f83944712441b5d86606d84
Anjola <anjola.awofisoye1@nhs.hnet>
2021-08-20 17:39:36 +0100

    update tf

--- main.tf
+++ main.tf
@@ -1,10 +1,41 @@
+
+terraform {
+  required_providers {
+    azurerm  = {
+      source  = "hashicorp/azurerm"
+      version = "=2.5.0"
+    }
+  }
+}
+
+
 provider "azurerm" {
-    version = "2.5.0"
     features {}
 }
 
-resource "azurern_resource_group" "tf_test" {
+resource "azurerm_resource_group" "tf_test" {
     name = "tfmainrg"
-    location = "United Kingdom"
+    location = "ukwest"
 }
 
+resource "azurerm_container_group" "tfcg_test" {
+  name                      = "cloudapi"
+  location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
+
+  ip_address_type           = "public"
+  dns_name_label            = "binarythislewa"
+  os_type                   = "Linux"
+
+  container {
+    name = "cloudapi"
+        image = "anjola/node-web-app" 
+        cpu = "1"
+        memory = "1"
+
+        ports {
+          port = 80
+          protocol = "TCP"
+        }
+  }
+}
